9122077b1d4b9616a89be0ea168024c9a746442f
alonsocamilla1 <alocamilla@gmail.com>
2023-03-13 17:24:20 -0300

    ClienteService do Lucas

--- projeto/src/main/java/br/gama/itau/projeto/Service/ClienteService.java
+++ projeto/src/main/java/br/gama/itau/projeto/Service/ClienteService.java
@@ -1,52 +1,30 @@
 package br.gama.itau.projeto.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.gama.itau.projeto.dto.ClienteDTO;
 import br.gama.itau.projeto.model.Cliente;
 import br.gama.itau.projeto.repositorio.ClienteRepo;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ClienteService {
 
     @Autowired
     private ClienteRepo repo;
- 
-    public Cliente cadastrarCliente(Cliente c) {
-        if(c.getId() > 0) {
-            return null;
-        }
-        Cliente clienteInserido = repo.save(c);
-        return clienteInserido;
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+        return repo.save(cliente);
     }
-    
-    public List<ClienteDTO> recuperarTodos() {
-        List<Cliente> lista = (List<Cliente>) repo.findAll();
-
-        List<ClienteDTO> listaDTO = new ArrayList<>();
-
-        for (Cliente c : lista) {
-            listaDTO.add(new ClienteDTO(c));
-        }
-
-        return listaDTO;
+    public List<Cliente> recuperarTodos() {
+        return (List<Cliente>) repo.findAll();
     }
 
-    public Cliente recuperarPeloID(int id) throws NotFoundException{
-        Optional<Cliente> clienteOptional = repo.findById(id);
+    public Cliente recuperarPeloID(int id) {
+        return repo.findById(id).orElse(null);}
 
-        if (clienteOptional.isEmpty()) {
-            throw new NotFoundException();
-        }
-
-        Cliente clienteEncontrado = clienteOptional.get();
-        return clienteEncontrado;
-    }
    
 }
