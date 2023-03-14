8321fad06192e8cabb3ef52d4fe7c6fbd93b989b
Emilly (Milhozinhozzz) <100964804+emilly-sasaki@users.noreply.github.com>
2023-03-14 11:17:11 -0300

    Comentários MovimentacaoRepo.java
    - Comentário adicionado

--- projeto/src/main/java/br/gama/itau/projeto/Repositorio/MovimentacaoRepo.java
+++ projeto/src/main/java/br/gama/itau/projeto/Repositorio/MovimentacaoRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.gama.itau.projeto.model.Movimentacao;
 
 public interface MovimentacaoRepo extends CrudRepository<Movimentacao, Integer>{
-    
+    // O repositório herda a interface CrudRepository com os métodos para fazer operações CRUD da Movimentacao
 }
