b5d53ef0821697f4d633f37f58b63ca45bb644ca
Emilly (Milhozinhozzz) <100964804+emilly-sasaki@users.noreply.github.com>
2023-03-14 15:01:54 -0300

    Comentários NotFoundException.java
    - Comentário adicionado

--- projeto/src/main/java/br/gama/itau/projeto/exception/NotFoundException.java
+++ projeto/src/main/java/br/gama/itau/projeto/exception/NotFoundException.java
@@ -2,6 +2,7 @@ package br.gama.itau.projeto.exception;
 
 public class NotFoundException extends RuntimeException {
     
+    // Método que mostra a mensagem personalizada da exceção
     public NotFoundException(String msg){
         super(msg);
     }
