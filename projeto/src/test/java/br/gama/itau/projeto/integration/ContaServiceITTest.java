7630cccef7d8367447eda69c934c2cfa549f3228
L199 <lucas.ferreira961245@gmail.com>
2023-03-15 08:33:33 -0300

    Vamos corrigir

---
+++ projeto/src/test/java/br/gama/itau/projeto/integration/ContaServiceITTest.java
@@ -0,0 +1,97 @@
+package br.gama.itau.projeto.integration;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.gama.itau.projeto.exception.NotFoundException;
+import br.gama.itau.projeto.Model.*;
+import br.gama.itau.projeto.Repositorio.*;
+import br.gama.itau.projeto.util.GenerateConta;
+import br.gama.itau.projeto.Service.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ContaServiceITTest {
+    
+    @InjectMocks
+    private ContaService service;
+    @Mock
+    private ContaRepo repo;
+    @Test
+    public void alterarDados_returnDadosAlterados_whenContaValida() {
+    BDDMockito.when(repo.findById(ArgumentMatchers.any(int.class)))
+                .thenReturn(Optional.of(GenerateConta.contaValida()));
+        BDDMockito.when(repo.save(ArgumentMatchers.any(Conta.class)))
+                .thenReturn(GenerateConta.contaValida2());
+        Conta contaParaAlterar = GenerateConta.contaValida2();
+        Conta dadosAlterados = service.alterarDados(contaParaAlterar);
+        // verificação
+        assertThat(dadosAlterados).isNotNull();
+        assertThat(dadosAlterados.getNumeroConta()).isEqualTo(2);
+        assertThat(dadosAlterados.getSaldo()).isEqualTo(contaParaAlterar.getSaldo());
+        // verifica se o método save foi chamado 1 vez
+        verify(repo, Mockito.times(1)).save(contaParaAlterar);
+    }
+    @Test
+    public void adicionarConta_returnContaAdicionada_whenContaValida() {
+        BDDMockito.when(repo.save(ArgumentMatchers.any(Conta.class)))
+                .thenReturn(GenerateConta.contaValida());
+        Conta novaConta = GenerateConta.novaContaToSave();
+       Conta contaAdicionada = service.adicionarConta(novaConta);
+        assertThat(contaAdicionada).isNotNull();
+        assertThat(contaAdicionada.getNumeroConta()).isPositive();
+        assertThat(contaAdicionada.getSaldo()).isEqualTo(novaConta.getSaldo());
+        verify(repo, Mockito.times(1)).save(novaConta);
+    }
+    @Test
+    public void adicionarConta_returnNull_whenContaInvalida() {
+        Conta contaValida = GenerateConta.contaValida();
+        Conta contaAdicionada = service.adicionarConta(contaValida);
+        assertThat(contaAdicionada).isNull();
+        verify(repo, Mockito.times(0)).save(contaValida);     
+    }
+    @Test
+    public void recuperarPeloNumero_returnConta_whenNumeroExist() {
+        BDDMockito.when(repo.findById(ArgumentMatchers.any(int.class)))
+                .thenReturn(Optional.of(GenerateConta.contaValida()));
+        Conta contaEncontrada = service.recuperarPeloNumero(2);
+        assertThat(contaEncontrada).isNotNull();
+        assertThat(contaEncontrada.getNumeroConta()).isGreaterThan(0);
+        assertThat(contaEncontrada.getNumeroConta())
+                .isEqualTo(GenerateConta.contaValida()
+                .getNumeroConta());
+    }
+    @Test
+    public void recuperarPeloNumero_throwException_whenNumeroNotExist() {
+        Conta contaValida = GenerateConta.novaContaToSave();
+        // verifica se uma exception do tipo NotFoundException é lançada
+        assertThrows(NotFoundException.class, () -> {
+            service.recuperarPeloNumero(contaValida.getNumeroConta());
+        });
+    }
+    @Test
+    public void recuperarContasPeloCliente_returnListaConta_whenIdExist() {
+        List<Conta> listaContas = new ArrayList<>();
+        listaContas.add(GenerateConta.contaValida());
+        listaContas.add(GenerateConta.contaValida2());
+        BDDMockito.when(repo.findAll()).thenReturn(listaContas);
+        List<Conta> listaRecuperada = service.recuperarContasPeloCliente(1);
+        assertThat(listaRecuperada).isNotNull();
+        assertThat(listaRecuperada).isNotEmpty();
+        assertThat(listaRecuperada.get(0).getNumeroConta()).isEqualTo(GenerateConta.contaValida().getNumeroConta());
+    }
+}
--- projeto/src/test/java/br/gama/itau/projeto/service/ClienteServiceTest.java
+++ projeto/src/test/java/br/gama/itau/projeto/service/ClienteServiceTest.java
@@ -1,72 +1,70 @@
 package br.gama.itau.projeto.service;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
 
-import java.util.Optional;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import br.gama.itau.projeto.Model.*;
-import br.gama.itau.projeto.Service.*;
-import br.gama.itau.projeto.Repositorio.*;
-import br.gama.itau.projeto.util.*;
-import br.gama.itau.projeto.exception.*;
-@ExtendWith(MockitoExtension.class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class ClienteServiceTest {
     
-    @InjectMocks
-    private ClienteService service;
-    @Mock
-    private ClienteRepo repo;
-    @Test
-    public void novoCliente_returnCadastrarCliente_whenClienteValido() {
-        BDDMockito.when(repo.save(ArgumentMatchers.any(Cliente.class)))
-                .thenReturn(GenerateCliente.clienteValido());
-        Cliente novoCliente = GenerateCliente.novoClienteToSave();
-        Cliente clienteCriado = service.cadastrarCliente(novoCliente);
-        assertThat(clienteCriado).isNotNull();
-        assertThat(clienteCriado.getIdCliente()).isPositive();
-        assertThat(clienteCriado.getIdCliente()).isEqualTo(GenerateCliente.clienteValido().getIdCliente());
-        // verifica se o método save foi chamado 1 vez
-        verify(repo, Mockito.times(1)).save(novoCliente);
-    }
-    @Test
-    public void novoCliente_returnNull_whenClienteInvalido() {
-       Cliente clienteValido = GenerateCliente.clienteValido();
-        Cliente clienteRetornado = service.cadastrarCliente(clienteValido);
-        assertThat(clienteRetornado).isNull();
-        // verifica que o método save não foi chamado
-        verify(repo, Mockito.times(1)).save(clienteValido);
-    }
-    @Test
-    public void recuperarPeloId_returnCliente_whenIdExist() {
-        BDDMockito.when(repo.findById(ArgumentMatchers.any(int.class)))
-                .thenReturn(Optional.of(GenerateCliente.clienteValido()));
-        Cliente clienteEncontrado = service.recuperarPeloId(1);
-        assertThat(clienteEncontrado)
-                .isNotNull();
-        assertThat(clienteEncontrado.getIdCliente())
-                .isGreaterThan(0);
-        assertThat(clienteEncontrado.getIdCliente())
-                .isEqualTo(GenerateCliente.clienteValido().getIdCliente())
-                .isNotNull();
-    }
-    @Test
-    public void recuperarPeloId_throwException_whenIdNotExist() {
-        Cliente clienteValido = GenerateCliente.novoClienteToSave();
-        // verifica se uma exception do tipo NotFoundException é lançada
-        // () -> { } é uma chamada de método anônimo
-        assertThrows(NotFoundException.class, () -> {
-            service.recuperarPeloId(clienteValido.getIdCliente());
-        });
-    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
