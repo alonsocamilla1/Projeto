a37180865eec4aeec502764fb88099a1827969f3
L199 <lucas.ferreira961245@gmail.com>
2023-03-15 13:03:49 -0300

    Atualizado Service (Lucas)

--- projeto/src/test/java/br/gama/itau/projeto/service/MovimentacaoServiceTest.java
+++ projeto/src/test/java/br/gama/itau/projeto/service/MovimentacaoServiceTest.java
@@ -1,12 +1,57 @@
 package br.gama.itau.projeto.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 
+//import java.util.Optional;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.gama.itau.projeto.util.GenerateMovi;
+import br.gama.itau.projeto.model.*;
+import br.gama.itau.projeto.repositorio.*;
+import br.gama.itau.projeto.service.*;
+
+@ExtendWith(MockitoExtension.class)
 public class MovimentacaoServiceTest {
 
-    
+    @InjectMocks
+    private MovimentacaoService service;
+    @Mock
+    private MovimentacaoRepo repo;
 
+    @Test
+    public void cadastrarMovimentacao_returnMovimentacaoCadastrada_whenMovimentacaoExist() {
+        BDDMockito.when(repo.save(ArgumentMatchers.any(Movimentacao.class)))
+                .thenReturn(GenerateMovi.moviValida());
+        Movimentacao novaMovi = GenerateMovi.novaMoviToSave();
+        Movimentacao moviCadastrada = service.cadastrarMovimentacao(novaMovi);
+        assertThat(moviCadastrada).isNotNull();
+        assertThat(moviCadastrada.getNumSeq()).isPositive();
+        assertThat(moviCadastrada.getValor()).isEqualTo(novaMovi.getValor());
+        verify(repo, Mockito.times(1)).save(novaMovi);
+    }
 
-    
+    @Test
+    public void cadastrarMovimentacao_returnNull_whenMovimentacaoNotExist() {
+        Movimentacao novaMovi = GenerateMovi.moviValida();
+        Movimentacao moviCadastrada = service.cadastrarMovimentacao(novaMovi);
+        assertThat(moviCadastrada).isNull();
+        verify(repo, Mockito.times(0)).save(novaMovi);
+    }
+
+    @Test
+    public void recuperarTodas_returnTodasAsMovimentacoesCadastradas_whenSucesso() {
+        BDDMockito.when(repo.save(ArgumentMatchers.any(Movimentacao.class)))
+                .thenReturn(GenerateMovi.moviValida());
+    }
+
 }
-
