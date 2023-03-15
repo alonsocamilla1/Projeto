ea89a81b2df05e865efc863910884ba160ec149d
L199 <lucas.ferreira961245@gmail.com>
2023-03-15 03:07:36 -0300

    Depois alterar

--- projeto/src/test/java/br/gama/itau/projeto/service/ClienteServiceTest.java
+++ projeto/src/test/java/br/gama/itau/projeto/service/ClienteServiceTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
